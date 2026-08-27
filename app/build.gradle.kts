fe8446b2439a98ed369dee8b7ae5768c4fcb68e6
吴思毅 <wusiyi@wusiyideMacBook-Pro.local>
2026-08-27 11:51:21 +0800

    chore: 版本升至 1.0.0，发布 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.wsy.ci"
         minSdk = 31
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += "arm64-v8a"
